fef8ba30ea6df63601360a5e3774d0247d09ecb2
Gismo <117986314+Gismo2000@users.noreply.github.com>
2024-03-28 13:07:07 +0100

    Delete anna directory
    ф

--- anna/src/components/DiscountCard/Discount.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DiscountCard = () => (
-  <div className="discount-card">
-    {/* <h2>DiscountCard</h2> */}
-  </div>
-);
-
-export default DiscountCard;
--- anna/src/components/ErrorPage/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ErrorPage = () => (
-  <div className="error">
-    <h2>УПППС... зайдите позже</h2>
-  </div>
-);
-
-export default ErrorPage;
